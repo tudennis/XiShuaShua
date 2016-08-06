6fb91f3d296cb0b5cbde65e020fecfa648a0d018
tudennis <tudennis@gmail.com>
2016-08-06 08:38:32 -0400

    hello world

---
+++ XiShuaShua/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  XiShuaShua
+//
+//  Created by Yijian on 8/6/16.
+//  Copyright © 2016 Yijian. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
